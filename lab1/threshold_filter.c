40e9838204de7d70caf158fccf7345a42e1dab9d
Vincent <vincent.aube@hotmail.com>
2016-04-26 02:04:18 +0200

    added threshold

---
+++ lab1/threshold_filter.c
@@ -0,0 +1,5 @@
+//
+// Created by Vincent Aube on 2016-04-26.
+//
+
+#include "threshold_filter.h"
---
+++ lab1/threshold_filter.h
@@ -0,0 +1,8 @@
+//
+// Created by Vincent Aube on 2016-04-26.
+//
+
+#ifndef LAB1_THRESHOLD_FILTER_H
+#define LAB1_THRESHOLD_FILTER_H
+
+#endif //LAB1_THRESHOLD_FILTER_H
