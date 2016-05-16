fe64085f60935df7b4d3ccd5420b98cd75309027
Vincent Aube <x_vinau@triolith2.(none)>
2016-05-17 00:12:20 +0200

    seems to work

--- lab3/laplsolv.f90
+++ lab3/laplsolv.f90
@@ -1,18 +1,20 @@
 program laplsolv
-use omp_lib
+
+  use omp_lib
+  implicit none
 !-----------------------------------------------------------------------
 ! Serial program for solving the heat conduction problem 
 ! on a square using the Jacobi method. 
 ! Written by Fredrik Berntsson (frber@math.liu.se) March 2003
 ! Modified by Berkant Savas (besav@math.liu.se) April 2006
 !-----------------------------------------------------------------------
-  integer, parameter                  :: n=1000, maxiter=1000
+  integer, parameter                  :: n=250, maxiter=1000
   double precision,parameter          :: tol=1.0E-3
   double precision,dimension(0:n+1,0:n+1) :: T
   double precision,dimension(n)       :: tmp1,tmp2,tmp3,left_col,right_col
-  double precision                    :: error,x
-  real                                :: t1,t0
-  integer                             :: i,j,k,cpu,nb_cpu
+  double precision                    :: error,local_error,x
+  double precision                    :: t1,t0
+  integer                             :: i,j,k,cpu,nb_cpu_max
   character(len=20)                   :: str
   integer, dimension(:), allocatable  :: from_col(:), to_col(:)
   integer                             :: reminder, current_col, chunk_size
@@ -25,22 +27,16 @@ use omp_lib
   T(0:n+1 , n+1)   = 1.0D0
   T(n+1   , 0:n+1) = 2.0D0
   
+  nb_cpu_max = omp_get_max_threads()
 
-  ! Solve the linear system of equations using the Jacobi method
-  call cpu_time(t0)
-  
-  !$omp parallel shared(nb_cpu)
-  nb_cpu = omp_get_num_threads()
-  !$omp end parallel
-
-  allocate(from_col(1:nb_cpu))
-  allocate(to_col(1:nb_cpu))
+  allocate(from_col(1:nb_cpu_max))
+  allocate(to_col(1:nb_cpu_max))
     
   current_col = 1
-  chunk_size = n / nb_cpu
-  reminder = modulo(n,nb_cpu)
+  chunk_size = n / nb_cpu_max
+  reminder = modulo(n,nb_cpu_max)
   
-  do cpu=1,nb_cpu
+  do cpu=1,nb_cpu_max
         from_col(cpu) = current_col
         to_col(cpu) = current_col + chunk_size - 1
         if (reminder > 0) then
@@ -49,7 +45,9 @@ use omp_lib
         end if
         current_col = to_col(cpu) + 1
   end do
-  
+
+  ! Solve the linear system of equations using the Jacobi method
+  t0 = omp_get_wtime()
   
   do k=1,maxiter
      error=0.0D0
@@ -58,6 +56,8 @@ use omp_lib
      !$omp default(private)     &
      !$omp shared(T,error,from_col,to_col)
      
+     local_error = 0.0D0
+
      cpu = omp_get_thread_num() + 1
      left_col = T(1:n,from_col(cpu)-1)
      right_col = T(1:n,to_col(cpu)+1)
@@ -77,9 +77,11 @@ use omp_lib
         T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+tmp3+tmp1)/4.0D0
         tmp1=tmp2
 
-        !$omp atomic 
-        error=max(error,maxval(abs(tmp2-T(1:n,j))))
+        local_error = max(local_error, maxval(abs(tmp2-T(1:n,j))))
      end do
+
+     !$omp atomic
+     error=max(local_error,error)
 
      !$omp end parallel
      
@@ -89,7 +91,7 @@ use omp_lib
      
   end do
   
-  call cpu_time(t1)
+  t1 = omp_get_wtime()
 
   write(unit=*,fmt=*) 'Time:',t1-t0,'Number of Iterations:',k
   write(unit=*,fmt=*) 'Temperature of element T(1,1)  =',T(1,1)
