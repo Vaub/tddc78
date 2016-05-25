b78bf12ab6c253aa00ac8633a7935cf7c8c0ff26
Vincent <vincent.aube@hotmail.com>
2016-05-26 00:09:07 +0200

    trying to work...

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -7,28 +7,13 @@
 #include "definitions.h"
 #include "mpi_env.h"
 
-#define NB_PARTICLES 10000
-#define NB_STEPS 10
+#define NB_PARTICLES 100
+#define NB_STEPS 1000
 
 static mpi_env_t env;
 
 double rand_range(void) {
     return (double) rand() / (double) RAND_MAX;
-}
-
-void send_to_neighbours(int flag, const mpi_env_t &env, int count, particle_t *particles) {
-
-    MPI_Request requests[3][3];
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            int nbr_rank = env.nbrs[i][j];
-            if (nbr_rank == -1 || nbr_rank == env.rank) { continue; }
-
-            MPI_Isend(particles, count, env.types.particle, nbr_rank, flag,
-                      env.grid_comm, &requests[i][j]);
-        }
-    }
-
 }
 
 std::vector<particle_t> receive_from_neighbours(int flag, const mpi_env_t &env) {
@@ -66,8 +51,8 @@ bool try_send_to_nbrs(const mpi_env_t& env, const cord_t &block,
                   0;
 
     if (shift_x == 0 && shift_y == 0) { return false; }
-    nbrs[1 + shift_x][1 + shift_y].push_back(particle);
 
+    nbrs[1 + shift_x][1 + shift_y].push_back(particle);
     return true;
 }
 
@@ -79,19 +64,21 @@ int main(int argc, char *argv[]) {
     float block_height = (float) ceil(BOX_VERT_SIZE / (float) env.grid_size[1]);
 
     cord_t block = {
-            .x0 = block_width * env.coords[0], .y0 = block_height * env.coords[1],
-            .x1 = block_width * env.coords[0] + block_width,
-            .y1 = block_height * env.coords[1] + block_height
+            .x0 = block_width * env.coords[0],
+            .y0 = block_height * env.coords[1],
+            .x1 = (block_width * env.coords[0]) + block_width,
+            .y1 = (block_height * env.coords[1]) + block_height
     };
     cord_t wall = {.x0 = 0, .y0 = 0, .x1 = BOX_HORIZ_SIZE, .y1 = BOX_VERT_SIZE};
 
-    std::vector<particle_t> particles((size_t)(NB_PARTICLES / env.nb_cpu));
+    // Init particles with random positions and velocity
+    std::list<particle_t> particles((size_t)(NB_PARTICLES / env.nb_cpu));
     for (auto& particle : particles) {
         double r = rand_range() * MAX_INITIAL_VELOCITY;
         double angle = rand_range() * (2*PI);
         pcord_t pcord = {
-                .x = block_width * (float)rand_range() + block.x0,
-                .y = block_height * (float)rand_range() + block.y0,
+                .x = (block_width * (float)rand_range()) + block.x0,
+                .y = (block_height * (float)rand_range()) + block.y0,
                 .vx = (float)(r * cos(angle)), .vy = (float)(r * sin(angle))
         };
 
@@ -102,35 +89,34 @@ int main(int argc, char *argv[]) {
     double t_start = MPI_Wtime();
     for (auto step = 0; step < NB_STEPS; ++step) {
 
+        // Particles sent to neighbours
         std::vector<particle_t> to_send_nbrs[3][3];
-
-        send_to_neighbours(FLAG_NBR_PARTICLES, env, (int)particles.size(), &particles[0]);
-        std::vector<particle_t> received = receive_from_neighbours(FLAG_NBR_PARTICLES, env);
 
         for (auto current = particles.begin(); current != particles.end();) {
             auto& particle = *current;
-            bool has_interacted = false;
+            bool has_collided = false;
 
-            for (auto i = std::next(current); i != particles.end() && !has_interacted; ++i) {
-                float collision = collide(&particle.pcord, &i->pcord);
-                if (collision == -1) { continue; }
+            // check collisions
+            for (auto with = std::next(current); with != particles.end() && !has_collided;) {
+                float collided_at = collide(&particle.pcord, &with->pcord);
+                if (collided_at == -1) {
+                    ++with;
+                    continue;
+                }
 
-                has_interacted = true;
-                interact(&particle.pcord, &i->pcord, collision);
-            }
-            for (auto i = received.begin(); i != received.end() && !has_interacted; ++i) {
-                float collision = collide(&particle.pcord, &i->pcord);
-                if (collision == -1) { continue; }
-
-                has_interacted = true;
-                interact(&particle.pcord, &i->pcord, collision);
+                interact(&particle.pcord, &with->pcord, collided_at);
+                particles.erase(with);
+                has_collided = true;
             }
 
-            if (!has_interacted) {
+            // move particles
+            if (!has_collided) {
                 feuler(&particle.pcord, 1);
             }
 
             local_pressure += wall_collide(&particle.pcord, wall);
+
+            // check if particle still in region, else send to neighbour
             if (try_send_to_nbrs(env, block, particle, to_send_nbrs)) {
                 current = particles.erase(current);
                 continue;
@@ -154,20 +140,19 @@ int main(int argc, char *argv[]) {
         std::vector<particle_t> new_particles = receive_from_neighbours(FLAG_NEW_PARTICLES, env);
         particles.insert(particles.end(), new_particles.begin(), new_particles.end());
 
-        //printf("[%d] \t Received %d particles, sent %d, now with %d total\n",
-        //       env.rank, (int)new_particles.size(), sent, (int)particles.size());
         MPI_Barrier(env.grid_comm);
-        //if (env.rank == ROOT_RANK) {
-        //    printf("\n=== ITER %d ===\n", step + 1);
-        //}
+        printf("[%d] \t Received %d particles, sent %d, now with %d total\n",
+               env.rank, (int)new_particles.size(), sent, (int)particles.size());
+
     }
 
+    MPI_Barrier(env.grid_comm);
     double pressure = 0;
     MPI_Reduce(&local_pressure, &pressure, 1, MPI_DOUBLE, MPI_SUM, ROOT_RANK, env.grid_comm);
 
     double t_end = MPI_Wtime();
     if(env.rank == ROOT_RANK){
-        pressure /= (NB_STEPS * (2 * BOX_HORIZ_SIZE + 2 * BOX_VERT_SIZE));
+        pressure /= ((2 * (BOX_HORIZ_SIZE + BOX_VERT_SIZE)));
         printf("Pressure : %f \t Time: %f\n", pressure,t_end-t_start);
     }
 
